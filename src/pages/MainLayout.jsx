307b60c652b01116712ea1e7218444f3b647561c
ovitormacel <ovitormacel00@gmail.com>
2024-01-14 16:05:28 -0300

    Inicio do Componente MainLayout

--- src/pages/MainLayout.jsx
+++ src/pages/MainLayout.jsx
@@ -1,9 +1,43 @@
-import { Outlet } from "react-router-dom";
+//ICONS
+import { FaSearch, FaHeart, FaShoppingCart } from "react-icons/fa";
+
+//STYLES
+import "./MainLayout.scss"
+
+import { Link, Outlet } from "react-router-dom";
 
 export default function MainLayout() {
     return (
         <>
-            <h1>MAIN LAYOUT</h1>
+            <header>
+                <div className="container">
+                    <nav className="nav-bar">
+                        <div className="left-side">
+                            <ul>
+                                <li><h1 className="logo">G</h1></li>
+                                <li><Link to={"/"} className="nav-link">Início</Link></li>
+                                <li><Link to={"/search"} className="nav-link">Explorar</Link></li>
+                            </ul>
+                            <form className="search-form">
+                                <label htmlFor="search" style={{'display': 'none'}}>Search</label>
+                                <input type="text" name="search" placeholder="Procurar Jogos..."/>
+                                <button type="submit"><FaSearch /></button>
+                            </form>
+                        </div>
+
+                        <div className="right-side">
+                            <ul>
+                                <li><button id="wishlist-icon" className="nav-bar-icon"><FaHeart /></button></li>
+                                <li><button id="cart-icon" className="nav-bar-icon"><FaShoppingCart /></button></li>
+                                <li>
+                                    <Link to={"/profile"}>Perfil</Link>
+                                </li>
+                            </ul>
+                        </div>
+                    </nav>
+                </div>
+            </header>
+
             <Outlet />
         </>
     )
